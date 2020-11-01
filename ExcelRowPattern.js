91a1d67ad30268399f40718ecea72daa0f518270
scakcin <65119283+scakcin@users.noreply.github.com>
2020-11-01 21:18:24 +0530

    Add files via upload

--- ExcelRowPattern.js
+++ ExcelRowPattern.js
@@ -2,11 +2,10 @@ const a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','
 var c=[];
 function asd(b){
     const aa = a.slice(0,Math.ceil(b/26));
-    aa.map(element => {
-        for(let i =0; i<26;i++){
-           c.push(element+a[i]);
-        }                
+    aa.map((element,i) =>{               
+        c.push(element+a.join(a[i]));
     });
-    return c.slice(0,b);  
+    return c;  
 }
-console.log(asd(200));
+let cs = (b)=>asd(b).join('').match(/.{1,2}(?=(.{2})+(?!.))|.{1,2}$/g).slice(0,b);
+console.log(cs(654));
